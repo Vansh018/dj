352be2a25020be98a38677b43959bd1ca96c82a2
Vansh018 <75937381+Vansh018@users.noreply.github.com>
2021-06-12 17:55:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
 
 function preload(){
     harry_porter = loadSound("music.mp3");
@@ -11,6 +17,8 @@ function setup(){
     video= createCapture(VIDEO);
     video.hide();
 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
     
 }
 
@@ -20,8 +28,18 @@ function draw(){
    
 }
 
+function modelLoaded(){
+    console.log("Model Is Initialized");
+}
 
-
+function gotPoses(results){
+    if(results.length > 0){
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
 
 
 
